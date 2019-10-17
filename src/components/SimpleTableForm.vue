79282ce3ef059698042229637c86ee0dd4ae5cb0
D. Lebedev <d.lebedev@arsenalins.ru>
2019-10-17 17:44:38 +0300

    simple table

--- src/components/SimpleTableForm.vue
+++ src/components/SimpleTableForm.vue
@@ -1,56 +1,72 @@
 <template>
   <div class="row form-wrapper">
-    <div class="col-md-6">
+    <div class="col-md-12">
+
       <form>
-        <div class="form-group">
-          <label for="dmn-table-name">Название DMN таблицы</label>
-          <input type="text" class="form-control" id="dmn-table-name" aria-describedby="" placeholder="Введите название">
-        </div>
-        <div class="form-group">
-          <label for="excel-file">Выберите файл</label>
-          <input type="file" class="form-control-file" id="excel-file">
-        </div>
+          <div class="form-group">
+            <label for="dmn-table-name">Название DMN таблицы</label>
+            <input type="text" class="form-control" id="dmn-table-name" aria-describedby="" placeholder="Введите название">
+          </div>
+          <div class="form-group">
+            <label for="excel-file">Выберите файл</label>
+            <input type="file" class="form-control-file" id="excel-file">
+          </div>
         <hr>
-        <strong>Входные параметры</strong> <button type="button" class="btn btn-primary">+</button>
-        <div>
-          <div class="form-group">
-            <label for="excel-col-name">Имя Excel столбца</label>
-            <input type="text" class="form-control" id="excel-col-name" aria-describedby="" placeholder="Введите имя столбца">
+
+        <div class="form-row">
+
+          <div class="col">
+            <h3>Входные параметры <button type="button" class="btn btn-primary">+</button></h3>
+            <div class="card">
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="excel-col-name">Имя Excel столбца</label>
+                  <input type="text" class="form-control" id="excel-col-name" aria-describedby="" placeholder="Введите имя столбца">
+                </div>
+                <div class="form-group">
+                  <label for="variable-name">Имя переменной</label>
+                  <input type="text" class="form-control" id="variable-name" aria-describedby="" placeholder="Введите имя переменной">
+                </div>
+                <div class="form-group">
+                  <label for="variable-type">Тип переменной</label>
+                  <select class="form-control" id="variable-type">
+                    <option>string</option>
+                    <option>int</option>
+                    <option>double</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="variable-name">Имя переменной</label>
-            <input type="text" class="form-control" id="variable-name" aria-describedby="" placeholder="Введите имя переменной">
+
+          <div class="col">
+            <h3>Выходные параметры <button type="button" class="btn btn-primary">+</button></h3>
+            <div class="card">
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="excel-col-name">Имя Excel столбца</label>
+                  <input type="text" class="form-control" id="excel-col-name" aria-describedby="" placeholder="Введите имя столбца">
+                </div>
+                <div class="form-group">
+                  <label for="variable-name">Имя переменной</label>
+                  <input type="text" class="form-control" id="variable-name" aria-describedby="" placeholder="Введите имя переменной">
+                </div>
+                <div class="form-group">
+                  <label for="variable-type">Тип переменной</label>
+                  <select class="form-control" id="variable-type">
+                    <option>string</option>
+                    <option>int</option>
+                    <option>double</option>
+                  </select>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="variable-type">Тип переменной</label>
-            <select class="form-control" id="variable-type">
-              <option>string</option>
-              <option>int</option>
-              <option>double</option>
-            </select>
-          </div>
+
         </div>
-        <hr>
-        <strong>Выходные параметры</strong> <button type="button" class="btn btn-primary">+</button>
-        <div>
-          <div class="form-group">
-            <label for="excel-col-name">Имя Excel столбца</label>
-            <input type="text" class="form-control" id="excel-col-name" aria-describedby="" placeholder="Введите имя столбца">
-          </div>
-          <div class="form-group">
-            <label for="variable-name">Имя переменной</label>
-            <input type="text" class="form-control" id="variable-name" aria-describedby="" placeholder="Введите имя переменной">
-          </div>
-          <div class="form-group">
-            <label for="variable-type">Тип переменной</label>
-            <select class="form-control" id="variable-type">
-              <option>string</option>
-              <option>int</option>
-              <option>double</option>
-            </select>
-          </div>
-        </div>
+
       </form>
+
     </div>
   </div>
 </template>
@@ -68,4 +84,5 @@ export default {
 .form-wrapper {
   margin: 20px 0;
 }
+
 </style>
